4ac870cbb58b9c8b7abbc1e07c83d5b30f9b7525
ahmedmokeddem <63117505+ahmedmokeddem@users.noreply.github.com>
2022-01-30 00:48:23 +0100

    fixed some details

--- src/commands/general/addcontent.js
+++ src/commands/general/addcontent.js
@@ -4,38 +4,38 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const fileContent = require("../../template/files_content.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("addcontenttopv")
-    .setDescription(" Add content to the PV ")
-    .addStringOption((option) =>
-      option
-        .setName("pvname")
-        .setDescription(" the name of the pv to add the content to ")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("content")
-        .setDescription("add your content")
-        .setRequired(true)
-    ),
+    data: new SlashCommandBuilder()
+        .setName("addcontenttopv")
+        .setDescription(" Add content to the PV ")
+        .addStringOption((option) =>
+            option
+            .setName("pvname")
+            .setDescription(" the name of the pv to add the content to ")
+            .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+            .setName("content")
+            .setDescription("add your content")
+            .setRequired(true)
+        ),
 
-  async execute(client, interaction, args) {
-    const content = interaction.options.getString("content");
-    const pvName = interaction.options.getString("pvname");
-    //if the file name doesn't exist
-    if (!fileContent[pvName])
-      return await interaction.reply("pv name doesn't exist!");
-    //set the constent prop
-    fileContent[pvName].content = content;
+    async execute(client, interaction, args) {
+        const content = interaction.options.getString("content");
+        const pvName = interaction.options.getString("pvname");
+        //if the file name doesn't exist
+        if (!fileContent[pvName])
+            return await interaction.reply("pv name doesn't exist!");
+        //set the constent prop
+        fileContent[pvName].content = content;
 
-    //reply
-    const embed1 = new discord.MessageEmbed()
-      .setDescription("content added succesfully")
-      .setColor("GREEN");
+        //reply
+        const embed1 = new discord.MessageEmbed()
+            .setDescription("The content has been successfully added to the PV " + fileContent[pvName].title)
+            .setColor("GREEN");
 
-    await interaction.reply({
-      embeds: [embed1],
-    });
-  },
+        await interaction.reply({
+            embeds: [embed1],
+        });
+    },
 };
--- src/commands/general/closepv.js
+++ src/commands/general/closepv.js
@@ -2,64 +2,62 @@ const discord = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const fileContent = require("../../template/files_content.js");
 const {
-  uploadFile,
-  read_files,
-  write_files,
-  generateUrl,
-  renderFile,
+    uploadFile,
+    read_files,
+    write_files,
+    generateUrl,
+    renderFile,
 } = require("../../../utils.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("closepv")
-    .setDescription("It creates the PV file")
-    .addStringOption((option) => {
-      return option
-        .setName("file_name")
-        .setDescription("Enter pv file name")
-        .setRequired(true);
-    }),
-  async execute(client, interaction, args) {
-    const file_name = interaction.options.getString("file_name");
+    data: new SlashCommandBuilder()
+        .setName("closepv")
+        .setDescription("It creates the PV file")
+        .addStringOption((option) => {
+            return option
+                .setName("file_name")
+                .setDescription("Enter pv file name")
+                .setRequired(true);
+        }),
+    async execute(client, interaction, args) {
+        const file_name = interaction.options.getString("file_name");
 
-    await interaction.deferReply();
-    //if the file name doesn't exist
-    if (!fileContent[file_name])
-      return await interaction.reply("pv name doesn't exist!");
-    //render the pv file
-    renderFile(file_name);
-    await interaction.editReply({
-      embeds: [
-        new discord.MessageEmbed()
-          .setDescription("Uploading file to drive ...")
-          .setColor("RANDOM"),
-      ],
-    });
-    /*let files = read_files();
-    let file;
-    if (files[file_name] === undefined) {
-      file = await uploadFile(file_name);
-      files[file.name] = file.id;
-      write_files(files);
-    }
+        await interaction.deferReply();
+        //if the file name doesn't exist
+        if (!fileContent[file_name]) { return await interaction.editReply("pv name doesn't exist!"); }
+        //render the pv file
+        renderFile(file_name);
+        await interaction.editReply({
+            embeds: [
+                new discord.MessageEmbed()
+                .setDescription("Uploading file to drive ...")
+                .setColor("RANDOM"),
+            ],
+        });
+        let files = read_files();
+        let file;
+        if (files[file_name] === undefined) {
+            file = await uploadFile(file_name);
+            files[file.name] = file.id;
+            write_files(files);
+        }
 
-    let link = await generateUrl(files[`${file_name}.docx`]);
-    
-    const embed1 = new discord.MessageEmbed()
-      .setDescription(
-        `The PV **${file_name}** has been successfully uploaded to drive`
-      )
-      .setAuthor({
-        name: "AutoPV",
-        iconURL: "https://i.ibb.co/rmGcG0Y/GDG-2-removebg-preview.jpg",
-      })
-      .setTitle(`${file_name}.docx`)
-      .setURL(`${link.webContentLink}`)
-      .setColor("GREEN");
+        let link = await generateUrl(files[`${file_name}.docx`]);
 
-    await interaction.editReply({
-      embeds: [embed1],
-    });
-    */
-  },
+        const embed1 = new discord.MessageEmbed()
+            .setDescription(
+                `The PV **${file_name}** has been successfully uploaded to drive`
+            )
+            .setAuthor({
+                name: "AutoPV",
+                iconURL: "https://i.ibb.co/rmGcG0Y/GDG-2-removebg-preview.jpg",
+            })
+            .setTitle(`${file_name}.docx`)
+            .setURL(`${link.webViewLink}`)
+            .setColor("GREEN");
+
+        await interaction.editReply({
+            embeds: [embed1],
+        });
+    },
 };
